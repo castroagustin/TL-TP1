d42793deb02e445db9c44563875767a071521ffd
Agustin Castro <agus.castro915@gmail.com>
2023-03-24 17:42:50 -0300

    v2

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    int num = 3, *pNum;
+
+    pNum = &num;
+
+    printf("Contenido del puntero: %p\n", *pNum);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", pNum);
+    printf("Direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria del puntero: %p\n", &pNum);
+    printf("Tamaño de memoria utilizado por la variable: %d\n", sizeof(num));
+
     printf("Hola mundo");
     return 0;
 }
